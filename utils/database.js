cde01940fc979a12c430f090449f26d2b239925f
mayank160903 <97187052+mayank160903@users.noreply.github.com>
2024-07-30 22:49:09 +0530

    initialization

---
+++ utils/database.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if(isConnected){
+        console.log("MongoDB is already connected");
+        return;
+    }
+    try{
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "share_prompt",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        isConnected = true;
+        console.log("MongoDB Connected!")
+    }catch(err){
+        console.log(err);
+    }
+}
+
+// mongodb pass: Z932cYwx8vAr6bQ5
